05ba8d3f3bd0e8642ded1fd10353dfc450ae8d58
Wendy Wilhelm <wendy.wilhelm@yahoo.com>
2024-11-03 18:40:28 -0500

    Player and Enemy Characters

---
+++ Source/Aura/Private/Character/AuraCharacterBase.cpp
@@ -0,0 +1,16 @@
+// Copyright Druid Mechanics
+
+
+#include "Character/AuraCharacterBase.h"
+
+AAuraCharacterBase::AAuraCharacterBase()
+{
+ 	PrimaryActorTick.bCanEverTick = false;
+
+}
+
+void AAuraCharacterBase::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
